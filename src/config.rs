68a759f1bb76742dd44e303e5480329f63d6e8cf
Jan Macku <jamacku@redhat.com>
2022-07-15 21:06:18 +0200

    Format source code using `cargo fmt`

--- src/config.rs
+++ src/config.rs
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier:  MIT
 
-use std::fs;
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::error::Error;
+use std::fs;
 use std::io;
 use std::io::Write;
-use std::string::ToString;
-use std::cmp::Ordering;
 use std::path::PathBuf;
-use std::collections::HashMap;
+use std::string::ToString;
 
 use ini::Ini;
-use regex::Regex;
 use libudev;
+use regex::Regex;
 
 use util::*;
 
-static NET_SETUP_LINK_CONF_DIR : &'static str = "/etc/systemd/network/";
-static LINK_FILE_PREFIX : &'static str = "71-net-ifnames-prefix-";
+static NET_SETUP_LINK_CONF_DIR: &'static str = "/etc/systemd/network/";
+static LINK_FILE_PREFIX: &'static str = "71-net-ifnames-prefix-";
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PrefixedLink {
@@ -36,7 +36,7 @@ impl PrefixedLink {
 
         let prefix = match RE.captures(&name) {
             Some(c) => c[1].to_string(),
-            None => "".to_string()
+            None => "".to_string(),
         };
 
         let i = name.trim_left_matches(&prefix).parse::<u64>()?;
@@ -44,13 +44,16 @@ impl PrefixedLink {
         let config = PrefixedLink {
             name: name,
             index: i,
-            hwaddr: hwaddr_from_event_device()?
+            hwaddr: hwaddr_from_event_device()?,
         };
 
         Ok(config)
     }
 
-    pub fn new_with_hwaddr<T: ToString>(link_name: &T, hwaddr: &T) -> Result<PrefixedLink, Box<Error>> {
+    pub fn new_with_hwaddr<T: ToString>(
+        link_name: &T,
+        hwaddr: &T,
+    ) -> Result<PrefixedLink, Box<Error>> {
         let addr = hwaddr_normalize(hwaddr)?;
         let name = link_name.to_string();
         PrefixedLink::link_name_sane(link_name)?;
@@ -61,7 +64,7 @@ impl PrefixedLink {
 
         let prefix = match RE.captures(&name) {
             Some(c) => c[1].to_string(),
-            None => "".to_string()
+            None => "".to_string(),
         };
         let i = name.trim_left_matches(&prefix).parse::<u64>()?;
 
@@ -101,7 +104,11 @@ impl PrefixedLink {
         let path = self.link_file_path();
         let mut link_file = fs::File::create(path)?;
 
-        write!(&mut link_file, "[Match]\nMACAddress={}\n\n[Link]\nName={}\n", self.hwaddr, self.name)?;
+        write!(
+            &mut link_file,
+            "[Match]\nMACAddress={}\n\n[Link]\nName={}\n",
+            self.hwaddr, self.name
+        )?;
 
         Ok(())
     }
@@ -110,9 +117,9 @@ impl PrefixedLink {
 impl Ord for PrefixedLink {
     fn cmp(&self, other: &PrefixedLink) -> Ordering {
         if self.index < other.index {
-            return Ordering::Less
+            return Ordering::Less;
         } else if self.index > other.index {
-            return Ordering::Greater
+            return Ordering::Greater;
         }
         Ordering::Equal
     }
@@ -124,10 +131,10 @@ impl PartialOrd for PrefixedLink {
     }
 }
 
-pub struct NetSetupLinkConfig  {
+pub struct NetSetupLinkConfig {
     config: HashMap<String, PrefixedLink>,
     links: Vec<PrefixedLink>,
-    ifname_prefix: String
+    ifname_prefix: String,
 }
 
 impl NetSetupLinkConfig {
@@ -165,10 +172,20 @@ impl NetSetupLinkConfig {
             return Ok(format!("{}{}", self.ifname_prefix, "0"));
         }
 
-        let last = self.links.last().ok_or("Failed to obtain last vector element")?;
-        let last_index = last.name.trim_left_matches(&self.ifname_prefix).parse::<u64>()?;
+        let last = self
+            .links
+            .last()
+            .ok_or("Failed to obtain last vector element")?;
+        let last_index = last
+            .name
+            .trim_left_matches(&self.ifname_prefix)
+            .parse::<u64>()?;
 
-        Ok(format!("{}{}", self.ifname_prefix, &(last_index + 1).to_string()))
+        Ok(format!(
+            "{}{}",
+            self.ifname_prefix,
+            &(last_index + 1).to_string()
+        ))
     }
 
     fn match_ethernet_links(udev_enumerate: &mut libudev::Enumerator) -> Result<(), Box<Error>> {
@@ -179,31 +196,37 @@ impl NetSetupLinkConfig {
     }
 
     fn enumerate_links_from_udev(&mut self) -> Result<(), Box<Error>> {
-        let udev  = libudev::Context::new()?;
+        let udev = libudev::Context::new()?;
         let mut enumerate = libudev::Enumerator::new(&udev)?;
         let mut links = Vec::new();
 
         NetSetupLinkConfig::match_ethernet_links(&mut enumerate)?;
 
         for device in enumerate.scan_devices()? {
-            let name = device.sysname().unwrap().to_str().ok_or("Failed to convert from ffi::OsStr to &str");
+            let name = device
+                .sysname()
+                .unwrap()
+                .to_str()
+                .ok_or("Failed to convert from ffi::OsStr to &str");
 
-            if ! name?.to_string().starts_with(&self.ifname_prefix) {
+            if !name?.to_string().starts_with(&self.ifname_prefix) {
                 continue;
             }
 
             // XXX: Move this to its own function and add more devtypes
             match device.devtype() {
-                Some(t) => {
-                    match t.to_str() {
-                        Some("vlan") | Some("bond") | Some("bridge") => continue,
-                        _ => {}
-                    }
-                }
+                Some(t) => match t.to_str() {
+                    Some("vlan") | Some("bond") | Some("bridge") => continue,
+                    _ => {}
+                },
                 None => {}
             }
 
-            let hwaddr = device.attribute_value("address").ok_or("Failed to read value of the 'address' sysfs attribute")?.to_str().ok_or("Failed to convert from ffi::OsStr to &str");
+            let hwaddr = device
+                .attribute_value("address")
+                .ok_or("Failed to read value of the 'address' sysfs attribute")?
+                .to_str()
+                .ok_or("Failed to convert from ffi::OsStr to &str");
             links.push(PrefixedLink::new_with_hwaddr(&name?, &hwaddr?)?);
         }
 
@@ -219,8 +242,8 @@ impl NetSetupLinkConfig {
             Ok(d) => d,
             Err(e) => match e.kind() {
                 io::ErrorKind::NotFound => return Ok(()),
-                _ => return Err(From::from(e))
-            }
+                _ => return Err(From::from(e)),
+            },
         };
 
         for f in files {
@@ -231,9 +254,13 @@ impl NetSetupLinkConfig {
 
             let path = entry.path();
             {
-                let name = path.file_name().ok_or("Failed to obtain filename")?.to_str().ok_or("Failed to convert OsStr to String")?;
+                let name = path
+                    .file_name()
+                    .ok_or("Failed to obtain filename")?
+                    .to_str()
+                    .ok_or("Failed to convert OsStr to String")?;
 
-                if ! name.starts_with(LINK_FILE_PREFIX) || ! name.ends_with(".link") {
+                if !name.starts_with(LINK_FILE_PREFIX) || !name.ends_with(".link") {
                     continue;
                 }
             }
@@ -243,21 +270,29 @@ impl NetSetupLinkConfig {
 
         for l in &link_files {
             let conf = Ini::load_from_file(l)?;
-            let match_section = conf.section(Some("Match".to_owned())).ok_or("Failed to parse link file, [Match] section not found")?;
-            let link_section = conf.section(Some("Link".to_owned())).ok_or("Failed to parse link file, [Link] section not found")?;
+            let match_section = conf
+                .section(Some("Match".to_owned()))
+                .ok_or("Failed to parse link file, [Match] section not found")?;
+            let link_section = conf
+                .section(Some("Link".to_owned()))
+                .ok_or("Failed to parse link file, [Link] section not found")?;
 
             let mac = match_section.get("MACAddress").ok_or("Failed to parse link file, \"MACAddress\"' option not present in the [Link] section")?;
-            let name = link_section.get("Name").ok_or("Failed to parse link file, \"Name\" option not present in the [Link] section")?;
+            let name = link_section.get("Name").ok_or(
+                "Failed to parse link file, \"Name\" option not present in the [Link] section",
+            )?;
 
-            if ! name.starts_with(&self.ifname_prefix) {
+            if !name.starts_with(&self.ifname_prefix) {
                 warn!("Unexpected link name");
                 continue;
             }
 
             let hwaddr = mac;
 
-            self.config.insert(hwaddr.to_string(), PrefixedLink::new(&name)?);
-            self.links.push(PrefixedLink::new_with_hwaddr(&name, &hwaddr)?);
+            self.config
+                .insert(hwaddr.to_string(), PrefixedLink::new(&name)?);
+            self.links
+                .push(PrefixedLink::new_with_hwaddr(&name, &hwaddr)?);
         }
         Ok(())
     }
@@ -269,12 +304,16 @@ mod tests {
     #[link(name = "umockdev")]
     extern "C" {
         fn umockdev_testbed_new() -> *mut UMockdevTestbed;
-        fn umockdev_testbed_add_from_string(testbed: *mut UMockdevTestbed, device_description: *mut c_char, err: *mut *mut u8);
+        fn umockdev_testbed_add_from_string(
+            testbed: *mut UMockdevTestbed,
+            device_description: *mut c_char,
+            err: *mut *mut u8,
+        );
     }
 
     use std::env;
-    use std::os::raw::c_char;
     use std::ffi::CString;
+    use std::os::raw::c_char;
     use std::path::Path;
 
     use super::*;
@@ -293,7 +332,8 @@ mod tests {
 
     #[test]
     fn prefixed_link_name_long() {
-        let config = PrefixedLink::new_with_hwaddr(&"neeeeeeeeeeeeeeeeeeeeeeeeeet0", &"ff:ff:ff:ff:ff:ff");
+        let config =
+            PrefixedLink::new_with_hwaddr(&"neeeeeeeeeeeeeeeeeeeeeeeeeet0", &"ff:ff:ff:ff:ff:ff");
         assert!(config.is_err());
     }
 
@@ -324,14 +364,13 @@ mod tests {
     fn mock_sysfs() -> Result<(), Box<Error>> {
         use std::io::prelude::*;
         use std::ptr;
-        let mut err : *mut u8 = ptr::null_mut();
+        let mut err: *mut u8 = ptr::null_mut();
 
         let mut net0 = fs::File::open("test/net0.mockdev").unwrap();
         let mut net0_device_description = String::new();
 
         let mut eth0 = fs::File::open("test/eth0.mockdev").unwrap();
         let mut eth0_device_description = String::new();
-
 
         net0.read_to_string(&mut net0_device_description).unwrap();
         eth0.read_to_string(&mut eth0_device_description).unwrap();
@@ -340,8 +379,16 @@ mod tests {
         env::set_var("DEVPATH", "/devices/pci0000:00/0000:00:03.0/net/eth0");
         unsafe {
             let test_bed = umockdev_testbed_new();
-            umockdev_testbed_add_from_string(test_bed, CString::new(net0_device_description).unwrap().into_raw(), &mut err);
-            umockdev_testbed_add_from_string(test_bed, CString::new(eth0_device_description).unwrap().into_raw(), &mut err);
+            umockdev_testbed_add_from_string(
+                test_bed,
+                CString::new(net0_device_description).unwrap().into_raw(),
+                &mut err,
+            );
+            umockdev_testbed_add_from_string(
+                test_bed,
+                CString::new(eth0_device_description).unwrap().into_raw(),
+                &mut err,
+            );
         }
 
         Ok(())
@@ -368,7 +415,10 @@ mod tests {
         mock_sysfs().unwrap();
 
         let prefixed_link = PrefixedLink::new(&"net1").unwrap();
-        assert_eq!(prefixed_link.link_file_path().as_path(), Path::new("/etc/systemd/network/71-net-ifnames-prefix-net1.link"));
+        assert_eq!(
+            prefixed_link.link_file_path().as_path(),
+            Path::new("/etc/systemd/network/71-net-ifnames-prefix-net1.link")
+        );
     }
 
     #[test]
--- src/sema.rs
+++ src/sema.rs
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier:  MIT
 
+use libc;
 use std::error::Error;
 use std::ffi::CString;
-use libc;
 
 #[derive(Debug)]
 pub struct Semaphore {
     raw_sema: *mut libc::sem_t,
-    name: CString
+    name: CString,
 }
 
 impl Semaphore {
@@ -16,13 +16,23 @@ impl Semaphore {
 
         let s;
         unsafe {
-            s = libc::sem_open(raw_sema_name.as_ptr() as *const i8, libc::O_CREAT, libc::S_IRUSR | libc::S_IWUSR, 1);
+            s = libc::sem_open(
+                raw_sema_name.as_ptr() as *const i8,
+                libc::O_CREAT,
+                libc::S_IRUSR | libc::S_IWUSR,
+                1,
+            );
             if s.is_null() {
-                return Err(From::from("Failed to allocate named semaphore, sem_open() failed"));
+                return Err(From::from(
+                    "Failed to allocate named semaphore, sem_open() failed",
+                ));
             }
         }
 
-        Ok(Semaphore{raw_sema: s, name: raw_sema_name})
+        Ok(Semaphore {
+            raw_sema: s,
+            name: raw_sema_name,
+        })
     }
 
     pub fn lock(&mut self) {
@@ -30,7 +40,6 @@ impl Semaphore {
             libc::sem_wait(self.raw_sema);
             debug!("lock taken by PID={}", libc::getpid());
         }
-
     }
 
     pub fn unlock(&mut self) {
@@ -52,9 +61,8 @@ impl Drop for Semaphore {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::{thread,time};
     use env_logger;
-
+    use std::{thread, time};
 
     #[test]
     fn sema_sanity() {
@@ -69,44 +77,48 @@ mod tests {
     fn sema_concurent() {
         let _ = env_logger::try_init();
         let sema = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
-        
-        let t1 = thread::Builder::new().spawn(|| {
-            warn!("T1 spawned");
-            let mut s = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
 
-            s.lock();
-            warn!("T1 in critical section");
-            thread::sleep(time::Duration::from_millis(100));
-            warn!("T1 leaving critical section");
-            s.unlock();
+        let t1 = thread::Builder::new()
+            .spawn(|| {
+                warn!("T1 spawned");
+                let mut s = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
 
-        }).unwrap();
+                s.lock();
+                warn!("T1 in critical section");
+                thread::sleep(time::Duration::from_millis(100));
+                warn!("T1 leaving critical section");
+                s.unlock();
+            })
+            .unwrap();
 
         thread::sleep(time::Duration::from_millis(50));
 
-        let t2 = thread::Builder::new().spawn(|| {
-            warn!("T2 spawned");
-            let mut s = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
+        let t2 = thread::Builder::new()
+            .spawn(|| {
+                warn!("T2 spawned");
+                let mut s = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
 
-            s.lock();
-            warn!("T2 in critical section");
-            thread::sleep(time::Duration::from_millis(3000));
-            warn!("T2 leaving critical section");
-            s.unlock();
-
-        }).unwrap();
+                s.lock();
+                warn!("T2 in critical section");
+                thread::sleep(time::Duration::from_millis(3000));
+                warn!("T2 leaving critical section");
+                s.unlock();
+            })
+            .unwrap();
 
         thread::sleep(time::Duration::from_millis(200));
 
-        let t3 = thread::Builder::new().spawn(|| {
-            warn!("T3 spawned");
-            let mut s = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
+        let t3 = thread::Builder::new()
+            .spawn(|| {
+                warn!("T3 spawned");
+                let mut s = Semaphore::new_with_name(&"test").expect("Failed to create semaphore");
 
-            s.lock();
-            warn!("T3 in critical section");
-            warn!("T3 leaving critical section");
-            s.unlock();
-        }).unwrap();
+                s.lock();
+                warn!("T3 in critical section");
+                warn!("T3 leaving critical section");
+                s.unlock();
+            })
+            .unwrap();
 
         t1.join().unwrap();
         t2.join().unwrap();
@@ -115,9 +127,7 @@ mod tests {
         unsafe {
             libc::sem_unlink(sema.name.as_ptr());
         }
-        
+
         // XXX: actually check that critical section where taken in right order
     }
-
-
 }
